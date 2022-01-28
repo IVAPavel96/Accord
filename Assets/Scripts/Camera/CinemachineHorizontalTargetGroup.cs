d6ac9a88e62028d43e4e77e575f3232a58d75f94
AlexProgrammer <alexprogrammer74@gmail.com>
2022-01-29 03:10:49 +0500

    Prop prefabs, level WIP, invert shader

---
+++ Assets/Scripts/Camera/CinemachineHorizontalTargetGroup.cs
@@ -0,0 +1,33 @@
+﻿using Cinemachine;
+using Extensions;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CinemachineHorizontalTargetGroup : CinemachineTargetGroup
+    {
+        void FixedUpdate()
+        {
+            float backup = transform.position.y;
+            if (m_UpdateMethod == UpdateMethod.FixedUpdate)
+                DoUpdate();
+            transform.position = transform.position.WithY(backup);
+        }
+
+        void Update()
+        {
+            float backup = transform.position.y;
+            if (!Application.isPlaying || m_UpdateMethod == UpdateMethod.Update)
+                DoUpdate();
+            transform.position = transform.position.WithY(backup);
+        }
+
+        void LateUpdate()
+        {
+            float backup = transform.position.y;
+            if (m_UpdateMethod == UpdateMethod.LateUpdate)
+                DoUpdate();
+            transform.position = transform.position.WithY(backup);
+        }
+    }
+}
--- Assets/Scripts/Extensions/Vector3Extensions.cs
+++ Assets/Scripts/Extensions/Vector3Extensions.cs
@@ -8,5 +8,20 @@ namespace Extensions
         {
             return new Vector2(vector.x, vector.y);
         }
+
+        public static Vector3 WithX(this Vector3 vector, float value)
+        {
+            return new Vector3(value, vector.y, vector.z);
+        }
+
+        public static Vector3 WithY(this Vector3 vector, float value)
+        {
+            return new Vector3(vector.x, value, vector.z);
+        }
+
+        public static Vector3 WithZ(this Vector3 vector, float value)
+        {
+            return new Vector3(vector.x, vector.y, value);
+        }
     }
 }
