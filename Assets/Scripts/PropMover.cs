b8d5b301785980255521febff66fb03d967caf14
Danila2903 <33436849+Danila2903@users.noreply.github.com>
2022-01-30 03:01:18 +0500

    Движущаяся платформа

--- Assets/Scripts/PropMover.cs
+++ Assets/Scripts/PropMover.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Extensions;
+using Game;
 using UnityEngine;
 
 public class PropMover : MonoBehaviour
@@ -9,14 +11,42 @@ public class PropMover : MonoBehaviour
     [SerializeField] private Transform movingObject;
     [SerializeField] private float duration;
 
+    private Rigidbody2D playerRigidbody2D;
+    private Vector3 lastFramePosition;
+    
+
     void Start()
     {
         StartMoving();
     }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entered: " + other.name);
+        if (other.CompareTag("Player"))
+        {
+            playerRigidbody2D = other.GetComponent<Rigidbody2D>();
+            lastFramePosition = transform.position;
+            
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var delta = transform.position - lastFramePosition;
+            playerRigidbody2D.position += delta.xy();
+            lastFramePosition = transform.position;
+        }
+    }
+    
+
     public void StartMoving()
     {
         movingObject.DOMove(p2.position, duration).From(p1.position).SetLoops(-1, LoopType.Yoyo);
     }
+
     
 }
