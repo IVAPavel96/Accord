8decab4666a3c766f2f12769a099ed7c622471d0
Danila2903 <33436849+Danila2903@users.noreply.github.com>
2022-01-29 13:58:50 +0500

    Анимации
    Анимации прыжка, передвижения,

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using Extensions;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.U2D;
 
 namespace Player
 {
@@ -11,6 +13,33 @@ namespace Player
         [SerializeField] private LayerMask groundCheckMask;
 
         private Rigidbody2D rb;
+        private Animator animator;
+        private SpriteRenderer renderer;
+        private bool wasGrounded; //нужна дл€ анимации завершени€ прыжка
+        private bool isHittingLeft;
+        private bool isHittingRight;
+        private bool isHittingWalls;
+        private float playerRadiusSqr;
+
+        #region отладка
+        private bool IsJumpingAnimation => animator != null && animator.GetBool("IsJumping"); //debug
+        [ShowInInspector]
+        private float IsMovingAnimation //debug
+        {
+            get
+            {
+                if (animator != null) return animator.GetFloat("Speed");
+                return 0f;
+            }
+        }
+
+        [ShowInInspector] private bool IsHittingLeft => isHittingLeft;
+        [ShowInInspector] private bool IsHittingRight => isHittingRight;
+        [ShowInInspector] private bool IsHittingWalls => isHittingWalls;
+
+        [ShowInInspector] private float distance;
+
+        #endregion
 
         private readonly float groundCheckRadius = 0.1f;
         Collider2D[] temp = new Collider2D[1];
@@ -27,12 +56,21 @@ namespace Player
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
+            animator = GetComponentInChildren<Animator>();
+            wasGrounded = Grounded;
+            renderer = GetComponentInChildren<SpriteRenderer>();
+            var circleR = GetComponent<CircleCollider2D>().radius;
+            playerRadiusSqr = circleR * circleR;
+            Physics2D.queriesStartInColliders = false;
         }
 
         private void Update()
         {
             Jump();
             MoveHorizontal();
+
+            CheckWallsCollision();
+            
         }
 
         private void Jump()
@@ -40,7 +78,19 @@ namespace Player
             if (Input.GetButtonDown("Jump") && Grounded)
             {
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                animator.SetBool("IsJumping", true);
             }
+            if (!wasGrounded && Grounded)
+            {
+                Debug.Log("landing");
+                OnLanding();
+            }
+            wasGrounded = Grounded;
+        }
+
+        public void OnLanding()
+        {
+            animator.SetBool("IsJumping", false); //вынес в отдельный метод - может еще что-нибудь понадобитс€
         }
 
         private void MoveHorizontal()
@@ -49,6 +99,29 @@ namespace Player
             Vector2 newVelocity = rb.velocity;
             newVelocity.x = inputDirection * horizontalVelocity;
             rb.velocity = newVelocity;
+
+            animator.SetFloat("Speed", Mathf.Abs(inputDirection));
+            if (Mathf.Abs(inputDirection) > 0.01f)
+                renderer.flipX = !(inputDirection > 0); //мен€ем ориентацию при смене направлени€
+        }
+
+        private void CheckWallsCollision()
+        {
+            isHittingRight = CheckHitFromDir(transform.right);
+            isHittingLeft = CheckHitFromDir(-transform.right);
+            isHittingWalls = isHittingRight || isHittingLeft;
+
+            animator.SetBool("IsHittingWalls", isHittingWalls);
+
+        }
+
+        private bool CheckHitFromDir(Vector2 direction)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+            if (hit.collider == null) return false;
+            
+            distance = Vector2.SqrMagnitude(hit.point - (Vector2)transform.position);
+            return distance <= playerRadiusSqr + 0.05f;
         }
     }
 }
