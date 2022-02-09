465540dc9229d0a8ab912ebce66e67b7f6b2494c
AlexProgrammer <alexprogrammer74@gmail.com>
2022-02-10 03:35:45 +0500

    UI fixes, tutorial level

--- Assets/Scripts/UI/HintPanel.cs
+++ Assets/Scripts/UI/HintPanel.cs
@@ -1,33 +1,43 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using Game;
 using Player;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace UI
 {
     public class HintPanel : MonoBehaviour
     {
-        [SerializeField] private Animator jumpButtonAnimator;
-        [SerializeField] private Animator leftButtonAnimator;
-        [SerializeField] private Animator rightButtonAnimator;
-        [SerializeField] private Animator useButtonAnimator;
-        [SerializeField] private Animator dPadAnimator;
+        // [SerializeField] private Animator jumpButtonAnimator;
+        // [SerializeField] private Animator leftButtonAnimator;
+        // [SerializeField] private Animator rightButtonAnimator;
+        // [SerializeField] private Animator useButtonAnimator;
+        // [SerializeField] private Animator dPadAnimator;
+        [SerializeField] private Image[] moveHighlight;
+        [SerializeField] private Image jumpHighlight;
+        [SerializeField] private Image useHighlight;
 
         [SerializeField] private UIWindow hintMove;
         [SerializeField] private UIWindow hintJump;
         [SerializeField] private UIWindow hintUse;
 
+        [SerializeField] private Color fadeColor = Color.gray;
+        [SerializeField] private int fadeDuration = 1;
+
         [Inject] private GameStateManager gameStateManager;
 
         private IEnumerable<PlayerTriggers> playerTriggersCached;
         private bool useHintShown;
+        private Sequence moveTweener;
+        private Tweener jumpTweener;
+        private Tweener useTweener;
 
-        // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
+            CreateTweeners();
             DisableBlinkingAnimations();
             hintMove.Show(0.2f);
             hintMove.onShow.AddListener(() => MoveControllersHighlight(true));
@@ -36,8 +46,10 @@ namespace UI
                 DisableBlinkingAnimations();
                 hintJump.Show(0.2f);
             });
-            hintJump.onShow.AddListener(() => jumpButtonAnimator.enabled = true);
+            // hintJump.onShow.AddListener(() => jumpButtonAnimator.enabled = true);
+            hintJump.onShow.AddListener(() => jumpTweener.Play());
             hintJump.onHide.AddListener(DisableBlinkingAnimations);
+            hintUse.onShow.AddListener(() => useTweener.Play());
             hintUse.onHide.AddListener(DisableBlinkingAnimations);
 
             playerTriggersCached = gameStateManager.Players.Select(player => player.GetComponent<PlayerTriggers>());
@@ -57,15 +69,33 @@ namespace UI
         private void DisableBlinkingAnimations()
         {
             MoveControllersHighlight(false);
-            useButtonAnimator.enabled = false;
-            jumpButtonAnimator.enabled = false;
+            // useButtonAnimator.enabled = false;
+            // jumpButtonAnimator.enabled = false;
+            jumpTweener.Rewind();
+            useTweener.Rewind();
         }
 
         private void MoveControllersHighlight(bool state)
         {
-            dPadAnimator.enabled = state;
-            leftButtonAnimator.enabled = state;
-            rightButtonAnimator.enabled = state;
+            // dPadAnimator.enabled = state;
+            // leftButtonAnimator.enabled = state;
+            // rightButtonAnimator.enabled = state;
+            if (state)
+                moveTweener.Play();
+            else
+                moveTweener.Rewind();
+        }
+
+        private void CreateTweeners()
+        {
+            moveTweener = DOTween.Sequence();
+            foreach (var image in moveHighlight)
+            {
+                moveTweener.Join(image.DOColor(fadeColor, fadeDuration).From(Color.white));
+            }
+            moveTweener.SetLoops(-1, LoopType.Yoyo);
+            jumpTweener = jumpHighlight.DOColor(fadeColor, fadeDuration).From(Color.white).SetLoops(-1, LoopType.Yoyo);
+            useTweener = useHighlight.DOColor(fadeColor, fadeDuration).From(Color.white).SetLoops(-1, LoopType.Yoyo);
         }
     }
 }
--- Assets/Scripts/UI/UIWindow.cs
+++ Assets/Scripts/UI/UIWindow.cs
@@ -1,4 +1,6 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,43 +9,66 @@ namespace UI
     public class UIWindow : MonoBehaviour
     {
         [SerializeField] private CanvasGroup window;
+        [SerializeField] private bool visibleOnAwake;
+        [SerializeField] private bool callEventsOnAwake;
         public UnityEvent onShow = new UnityEvent();
 
         public UnityEvent onHide = new UnityEvent();
+        private Tweener tweener;
 
-        private void Start()
+        private void Awake()
         {
-            SetAlpha(0, 0);
+            if (visibleOnAwake)
+                Show(0, callEventsOnAwake);
+            else
+                Hide(0, callEventsOnAwake);
+        }
+
+        private void OnDestroy()
+        {
+            tweener?.Kill();
         }
 
         public void Show(float fadeIn = 0)
         {
-            SetAlpha(1, fadeIn);
-            Invoke(nameof(TriggerOnShow), fadeIn);
-            window.blocksRaycasts = true;
+            Show(fadeIn, true);
         }
 
-        private void TriggerOnShow()
+        public void Show(float fadeIn, bool useCallback)
         {
-            onShow.Invoke();
+            if (useCallback)
+                SetAlphaSmooth(1, fadeIn, () => onShow.Invoke());
+            else
+                SetAlphaSmooth(1, fadeIn, null);
+            window.blocksRaycasts = true;
         }
 
         public void Hide(float fadeOut = 0)
         {
-            SetAlpha(0, fadeOut);
-            Invoke(nameof(TriggerOnHide), fadeOut);
+            Hide(fadeOut, true);
+        }
+
+        public void Hide(float fadeOut, bool useCallback)
+        {
+            if (useCallback)
+                SetAlphaSmooth(0, fadeOut, () => onHide.Invoke());
+            else
+                SetAlphaSmooth(0, fadeOut, null);
             window.blocksRaycasts = false;
         }
 
-        private void TriggerOnHide()
+        private void SetAlphaSmooth(float value, float duration, [CanBeNull] Action callback)
         {
-            onHide.Invoke();
-        }
+            if (tweener != null && !tweener.IsComplete())
+            {
+                tweener.Complete();
+                tweener.Kill();
+                tweener = null;
+            }
 
-        private void SetAlpha(float value, float duration)
-        {
             if (duration > 0)
-                window.DOFade(value, duration).SetEase(Ease.OutSine);
+                tweener = window.DOFade(value, duration).SetEase(Ease.OutSine)
+                    .OnComplete(() => callback?.Invoke()).SetAutoKill();
             else
                 window.alpha = value;
         }
